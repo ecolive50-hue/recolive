acc8a72f9f2971c7d7ec918f98474e55cbd6db47
ecolive50-hue <ecolive50@gmail.com>
2025-12-17 22:30:57 +0600

    Create api_service.dart

---
+++ services/api_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../core/constants.dart';
+import '../models/user_model.dart';
+
+class ApiService {
+  static Future<UserModel> getProfile(String userId, String name) async {
+    final res = await http.post(
+      Uri.parse('${AppConstants.baseUrl}/user'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'userId': userId, 'name': name}),
+    );
+    return UserModel.fromJson(jsonDecode(res.body));
+  }
+
+  static Future<int> addCoins(String userId, int amount) async {
+    final res = await http.post(
+      Uri.parse('${AppConstants.baseUrl}/add-coins'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'userId': userId, 'amount': amount}),
+    );
+    return jsonDecode(res.body)['coins'];
+  }
+}
