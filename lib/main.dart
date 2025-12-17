2e13205032485c530db4bcf91ccc89e4b37c1341
ecolive50-hue <ecolive50@gmail.com>
2025-12-17 12:31:24 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RecoLiveApp());
+}
+
+class RecoLiveApp extends StatelessWidget {
+  const RecoLiveApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RecoLive',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('RecoLive'),
+        ),
+        body: const Center(
+          child: Text(
+            'RecoLive App Ready 🚀',
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    );
+  }
+}
