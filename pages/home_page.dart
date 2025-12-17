eb9bf88769f869ccdb153977061cbfdf0c356abd
ecolive50-hue <ecolive50@gmail.com>
2025-12-17 22:35:21 +0600

    Create home_page.dart

---
+++ pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(child: Text('Welcome to RecoLive')),
+    );
+  }
+}
